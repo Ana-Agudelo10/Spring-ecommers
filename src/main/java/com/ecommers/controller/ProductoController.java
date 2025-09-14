ef1f7da4c5b3d7830cfe781e8daa7cdf0de04c8e
Ana Agudelo <valeagudelob10@gmail.com>
2025-09-14 12:18:11 -0500

    Implementar las funcionalidades de cargar imagen y mostrar imagen para el administrador

--- src/main/java/com/ecommers/controller/ProductoController.java
+++ src/main/java/com/ecommers/controller/ProductoController.java
@@ -76,13 +76,13 @@ public class ProductoController {
 	
 	@PostMapping("/update")
 	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		Producto p= new Producto();
+		p=productoService.get(producto.getId()).get();
+		
 		if(file.isEmpty()) { // Editamos el producto pero no cambiamos la imagen
-			Producto p= new Producto();
-			p=productoService.get(producto.getId()).get();
+			
 			producto.setImagen(p.getImagen());
 		} else {
-			Producto p = new Producto();
-			p=productoService.get(producto.getId()).get();
 			
 			//Eliminar cuando no sea la imagen por defecto
 			if(!p.getImagen().equals("default.jpg")) {
@@ -93,6 +93,7 @@ public class ProductoController {
 			producto.setImagen(nombreImagen);
 		}
 		
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
